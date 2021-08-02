2fd0db5322fa1a88e2e9b226ed55421044cebef9
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-02 08:52:58 +0000

    // 10170번 NFC West vs North

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
-// 10926번 ??!
+// 10170번 NFC West vs North
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
-	string id;
 
-	cin >> id;
-	cout << id + "??!";
+	cout << "NFC West       W   L  T\n";
+	cout << "-----------------------\n";
+	cout << "Seattle        13  3  0\n";
+	cout << "San Francisco  12  4  0\n";
+	cout << "Arizona        10  6  0\n";
+	cout << "St. Louis      7   9  0\n";
+	cout << "\n";
+	cout << "NFC North      W   L  T\n";
+	cout << "-----------------------\n";
+	cout << "Green Bay      8   7  1\n";
+	cout << "Chicago        8   8  0\n";
+	cout << "Detroit        7   9  0\n";
+	cout << "Minnesota      5  10  1\n";
 
 	return 0;
 }
