f60ef72c4e6f6ef8725bafcb246ee5737682387f
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-02 08:41:11 +0000

    // 5339번 콜센터

--- 2108/0802.cpp
+++ 2108/0802.cpp
@@ -76,3 +76,23 @@ int main() {
 	
 	return 0;
 }
+
+// 5339번 콜센터
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "     /~\\\n";
+	cout << "    ( oo|\n";
+	cout << "    _\\=/_\n";
+	cout << "   /  _  \\\n";
+	cout << "  //|/.\\|\\\\\n";
+	cout << " ||  \\ /  ||\n";
+	cout << "============\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
-// 9654번 나부 함대 데이터
+// 5339번 콜센터
 #include <iostream>
 using namespace std;
 
 int main() {
 	
-	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
-	cout << "N2 Bomber      Heavy Fighter  Limited    21        \n";
-	cout << "J-Type 327     Light Combat   Unlimited  1         \n";
-	cout << "NX Cruiser     Medium Fighter Limited    18        \n";
-	cout << "N1 Starfighter Medium Fighter Unlimited  25        \n";
-	cout << "Royal Cruiser  Light Combat   Limited    4         \n";
+	cout << "     /~\\\n";
+	cout << "    ( oo|\n";
+	cout << "    _\\=/_\n";
+	cout << "   /  _  \\\n";
+	cout << "  //|/.\\|\\\\\n";
+	cout << " ||  \\ /  ||\n";
+	cout << "============\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
 	
 	return 0;
 }
