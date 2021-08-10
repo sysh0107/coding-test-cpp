3c677665b38866f76b2dd2bee31659cd5f36d3a4
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-10 04:16:17 +0000

    // 2420번 사파리월드

--- 2108/0810.cpp
+++ 2108/0810.cpp
@@ -103,3 +103,22 @@ int main() {
 
 	return 0;
 }
+// 2420번 사파리월드
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	long long a, b = 0;
+	long result = 0;
+	
+	cin >> a>>b;
+
+	result = a - b;
+
+	if(result > 0) cout << result;
+	else cout << -result;
+
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
-// 10156번 과자
+// 2420번 사파리월드
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
 	
-	int a, b, c = 0;
-	int result = 0;
+	long long a, b = 0;
+	long result = 0;
 	
-	cin >> a>>b>>c;
+	cin >> a>>b;
 
-	result = a * b - c;
+	result = a - b;
 
-	if(result > 0){
-		cout << result;
-	}
-	else {
-		cout << 0;
-	}
+	if(result > 0) cout << result;
+	else cout << -result;
 
 	return 0;
 }
