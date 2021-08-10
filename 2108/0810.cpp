46225c9d1a311dcfac2ecb7b34e7140b23121d21
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-10 05:58:58 +0000

    // 5086번 배수와 약수

--- 2108/0810.cpp
+++ 2108/0810.cpp
@@ -232,3 +232,31 @@ int main() {
 
 	return 0;
 }
+// 5086번 배수와 약수
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main() {
+	
+	while(1){
+		int a, b = 0;
+
+		cin >> a>>b;
+		if(a == 0){
+			break;
+		}
+		else if(a % b == 0){
+			cout << "multiple\n";
+		}
+		else if(b % a == 0){
+			cout << "factor\n";
+		}
+		else{
+			cout << "neither\n";
+		}
+	}
+	
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 1009번 분산처리
+// 5086번 배수와 약수
 #include <iostream>
 #include <string>
 #include <math.h>
@@ -6,24 +6,23 @@ using namespace std;
 
 int main() {
 	
-	int inp[7], sum = 0;
-	int min = 100;
-	for(int i = 0; i < 7; i++){
-		cin >> inp[i];
-		if(inp[i] % 2 == 1){
-			sum += inp[i];
-			if(min > inp[i]){
-				min = inp[i];
-			}
+	while(1){
+		int a, b = 0;
+
+		cin >> a>>b;
+		if(a == 0){
+			break;
+		}
+		else if(a % b == 0){
+			cout << "multiple\n";
+		}
+		else if(b % a == 0){
+			cout << "factor\n";
+		}
+		else{
+			cout << "neither\n";
 		}
 	}
-	if(sum == 0){
-		cout <<"-1";
-	}
-	else{
-		cout << sum<<"\n"<<min;
-	}
 	
-
 	return 0;
 }
