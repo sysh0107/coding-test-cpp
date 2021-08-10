583a5b053768de5473a581122ef8a6dfaae218df
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-10 05:38:34 +0000

    // 1009번 분산처리

--- 2108/0810.cpp
+++ 2108/0810.cpp
@@ -203,3 +203,32 @@ int main() {
 
 	return 0;
 }
+// 1009번 분산처리
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main() {
+	
+	int inp[7], sum = 0;
+	int min = 100;
+	for(int i = 0; i < 7; i++){
+		cin >> inp[i];
+		if(inp[i] % 2 == 1){
+			sum += inp[i];
+			if(min > inp[i]){
+				min = inp[i];
+			}
+		}
+	}
+	if(sum == 0){
+		cout <<"-1";
+	}
+	else{
+		cout << sum<<"\n"<<min;
+	}
+	
+
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -6,25 +6,24 @@ using namespace std;
 
 int main() {
 	
-	int T = 0;
-	cin >> T;
-
-	for(int i = 0; i < T; i++){
-		
-		int a, b = 0;
-		int result = 1;
-		cin >> a>>b;
-
-		for(int j = 0; j < b; j++){
-			result *= a;
-			result %= 10;
-			//cout << result<<"_";
+	int inp[7], sum = 0;
+	int min = 100;
+	for(int i = 0; i < 7; i++){
+		cin >> inp[i];
+		if(inp[i] % 2 == 1){
+			sum += inp[i];
+			if(min > inp[i]){
+				min = inp[i];
+			}
 		}
-		if(result == 0){
-			result = 10;
-		}
-		cout << result<<"\n";
 	}
+	if(sum == 0){
+		cout <<"-1";
+	}
+	else{
+		cout << sum<<"\n"<<min;
+	}
+	
 
 	return 0;
 }
