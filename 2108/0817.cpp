4e1c5aaae329c4435aeaaf03fcdce9a4d1179cab
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-17 04:03:37 +0000

    // 10992번 별 찍기 - 17

--- 2108/0817.cpp
+++ 2108/0817.cpp
@@ -20,3 +20,38 @@ int main() {
 
 	return 0;
 }
+
+// 10992번 별 찍기 - 17
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	
+	int N = 0;
+	cin >> N;
+
+	for(int i = 0; i < N - 1; i++){
+		for(int j = N - i - 1; j > 0; j--){
+			cout << ' ';
+		}
+		cout << '*';
+		for(int k = 0; k < i * 2 - 1; k++){
+			cout << ' ';
+		}
+		if(i != 0){
+			cout << '*';
+		}
+
+		cout << '\n';
+	}
+
+	for(int i = 0; i < N*2 - 1; i++){
+		cout << "*";
+	}
+	
+
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 5565번 영수증
+// 10992번 별 찍기 - 17
 #include <iostream>
 #include <string>
 #include <math.h>
@@ -7,14 +7,28 @@ using namespace std;
 
 int main() {
 	
-	int sum = 0;
-	cin >> sum;
-	int books[9];
-	for(int i = 0; i < 9; i++){
-		cin >> books[i];
-		sum -= books[i];
+	int N = 0;
+	cin >> N;
+
+	for(int i = 0; i < N - 1; i++){
+		for(int j = N - i - 1; j > 0; j--){
+			cout << ' ';
+		}
+		cout << '*';
+		for(int k = 0; k < i * 2 - 1; k++){
+			cout << ' ';
+		}
+		if(i != 0){
+			cout << '*';
+		}
+
+		cout << '\n';
 	}
-	cout << sum;
+
+	for(int i = 0; i < N*2 - 1; i++){
+		cout << "*";
+	}
+	
 
 	return 0;
 }
