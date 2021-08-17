2da2c6e61a4b1642d32d6411c10a875cf40256d8
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-17 04:30:23 +0000

    // 10162번 전자레인지

--- 2108/0817.cpp
+++ 2108/0817.cpp
@@ -86,3 +86,29 @@ int main() {
 	
 	return 0;
 }
+
+// 10162번 전자레인지
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	
+	int five = 300;
+	int one = 60;
+	int ten = 10;
+
+	int N = 0;
+	cin >> N;
+
+	if(N % 10 != 0)cout << -1;
+	else{
+		cout << N / five << ' ';
+		cout << (N % five) / one << ' ';
+		cout << (N % one) / ten;
+	}
+	
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 10991번 별 찍기 - 16
+// 10162번 전자레인지
 #include <iostream>
 #include <string>
 #include <math.h>
@@ -7,23 +7,18 @@ using namespace std;
 
 int main() {
 	
+	int five = 300;
+	int one = 60;
+	int ten = 10;
+
 	int N = 0;
 	cin >> N;
 
-	for(int i = 0; i < N; i++){
-		for(int j = N - i - 1; j > 0; j--){
-			cout << ' ';
-		}
-		for(int k = 0; k < i * 2 + 1; k++){
-			if(k % 2 == 0){
-				cout << '*';
-			}
-			else{
-				cout << ' ';
-			}
-		}
-
-		cout << '\n';
+	if(N % 10 != 0)cout << -1;
+	else{
+		cout << N / five << ' ';
+		cout << (N % five) / one << ' ';
+		cout << (N % one) / ten;
 	}
 	
 	return 0;
