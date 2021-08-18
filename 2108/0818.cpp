ac28c01d2ac3a29dd2c264b853d4709ad53b5d6d
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-18 01:34:02 +0000

    // 2953번 나는 요리사다

--- 2108/0818.cpp
+++ 2108/0818.cpp
@@ -38,3 +38,34 @@ int main() {
 	
 	return 0;
 }
+
+// 2953번 나는 요리사다
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int score[5][4];
+	int winner, winner_score = 0;
+	int store = 0;
+
+	for(int i = 0; i < 5; i++){
+		int store = 0;
+		for(int j = 0; j < 4; j++){
+			cin >> score[i][j];
+			store += score[i][j];
+		}
+		if(store > winner_score){
+			winner = i + 1;
+			winner_score = store;
+		}
+	}
+	cout << winner <<' '<< winner_score;
+	
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 1373번 2진수 8진수
+// 2953번 나는 요리사다
 #include <iostream>
 #include <string>
 using namespace std;
@@ -9,30 +9,22 @@ int main() {
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	string eight;
-	cin >> eight;
-	int len = eight.size();
+	int score[5][4];
+	int winner, winner_score = 0;
+	int store = 0;
 
-	if(eight[0] == '0')cout << "0";
-	else if(eight[0] == '1')cout << "1";
-	else if(eight[0] == '2')cout << "10";
-	else if(eight[0] == '3')cout << "11";
-	else if(eight[0] == '4')cout << "100";
-	else if(eight[0] == '5')cout << "101";
-	else if(eight[0] == '6')cout << "110";
-	else if(eight[0] == '7')cout << "111";
-
-	for(int i = 1; i < len; i++){
-		if(eight[i] == '0')cout << "000";
-		else if(eight[i] == '1')cout << "001";
-		else if(eight[i] == '2')cout << "010";
-		else if(eight[i] == '3')cout << "011";
-		else if(eight[i] == '4')cout << "100";
-		else if(eight[i] == '5')cout << "101";
-		else if(eight[i] == '6')cout << "110";
-		else if(eight[i] == '7')cout << "111";
+	for(int i = 0; i < 5; i++){
+		int store = 0;
+		for(int j = 0; j < 4; j++){
+			cin >> score[i][j];
+			store += score[i][j];
+		}
+		if(store > winner_score){
+			winner = i + 1;
+			winner_score = store;
+		}
 	}
-
+	cout << winner <<' '<< winner_score;
 	
 	return 0;
 }
