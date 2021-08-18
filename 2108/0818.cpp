b51d8c6dfb6aa38213c846a48cd1c95e9d22173f
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-08-18 04:26:32 +0000

    // 1100번 하얀 칸

--- 2108/0818.cpp
+++ 2108/0818.cpp
@@ -118,3 +118,48 @@ int main() {
 	
 	return 0;
 }
+// 1076번 저항
+
+// 2902번 KMP는 왜 KMP일까?
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string str, result;
+	cin >> str;
+
+	result += str[0];
+
+	for(int i = 1; i < str.size(); i++){
+		if(isupper(str[i])){
+			result += str[i];
+		}
+	}
+	cout << result;
+	return 0;
+}
+// 1100번 하얀 칸
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	char board[8][8];
+	int result = 0;
+
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			cin >> board[i][j];
+			if((i%2 == 0&&j%2 == 0&&board[i][j] == 'F') ||
+			(i%2 == 1&&j%2 == 1&&board[i][j] == 'F')){
+				result += 1;
+			}
+		}
+	}
+	cout << result;
+	
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
-// 10820번 문자열 분석
+// 1100번 하얀 칸
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
 
-	string str;
+	char board[8][8];
+	int result = 0;
 
-	while(getline(cin, str)){
-		int lower = 0, upper = 0, num = 0, space = 0;
-
-		for(int i = 0; i < str.size(); i++){
-			if(islower(str[i]))lower += 1;
-			else if(isupper(str[i]))upper += 1;
-			else if(isdigit(str[i]))num += 1;
-			else if(isspace(str[i]))space += 1;
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			cin >> board[i][j];
+			if((i%2 == 0&&j%2 == 0&&board[i][j] == 'F') ||
+			(i%2 == 1&&j%2 == 1&&board[i][j] == 'F')){
+				result += 1;
+			}
 		}
-		cout << lower<<' '<<upper<<' '<<num<<' '<<space<<'\n';
 	}
+	cout << result;
 	
 	return 0;
 }
