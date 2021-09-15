3dd0aa9015f8ad346c78a916b9d5d44590d291c2
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2021-09-15 10:55:06 +0000

    // 1373번 2진수 8진수

--- 2109/0915.cpp
+++ 2109/0915.cpp
@@ -12,14 +12,20 @@ int main() {
 	string num;
 	cin >> num;
 
+	if(num == "0"){
+	    cout << '0';
+	    return 0;
+	}
+
 	if(num.length() % 3 == 1){
-		cout << num[1] - '0';
+		cout << num[0] - '0';
 	}
 	else if(num.length() % 3 == 2){
 		cout << (num[0] - '0') * 2 + num[1] - '0';
 	}
 
 	for(int i = num.length() % 3; i < num.length(); i += 3){
+
 		cout << (num[i] - '0') * 4 + (num[i + 1] - '0') * 2 + num[i + 2] - '0';
 	}
 	
